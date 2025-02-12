4d4dea53d5e65314fe17179a89bd9da307571571
Abdallah Ahmed <aametwally@ejada.com>
2025-02-12 02:25:36 +0200

    add search bar component

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextInput from '../TextInput';
+
+interface SearchBarProps {
+  value: string;
+  setValue: (val: string) => void;
+}
+export default function index({value, setValue}: SearchBarProps) {
+  return <TextInput value={value} setValue={setValue} isSearchBar />;
+}
