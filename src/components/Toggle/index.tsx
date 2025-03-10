7443f30014e0ba41249b09ea3043ab210a84775f
Abdallah Ahmed <aametwally@ejada.com>
2025-03-10 15:10:13 +0200

    add toggle component

---
+++ src/components/Toggle/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ToggleSwitch from 'toggle-switch-react-native';
+import {appColors} from '../../theme/colors';
+
+interface ToggleProps {
+  isOn: boolean;
+  onToggle: ((isOn: boolean) => any) | undefined;
+}
+const Toggle = ({isOn, onToggle}: ToggleProps) => {
+  return (
+    <ToggleSwitch
+      isOn={isOn}
+      onColor={appColors.orange}
+      offColor={appColors.gray100}
+      size="medium"
+      onToggle={onToggle}
+      animationSpeed={200}
+    />
+  );
+};
+
+export default Toggle;
